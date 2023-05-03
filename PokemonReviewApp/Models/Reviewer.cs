4c838b4549b600bb694af8aa114a3ba1e9aa4435
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-03 02:36:44 +0200

    Models relations | Relations between models one to many and one to one

--- PokemonReviewApp/Models/Reviewer.cs
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -6,5 +6,6 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public ICollection<Review> Reviews { get; set;}
     }
 }
